6b109612f47bf338beb42660896bbd148e58a121
Evgeniy Nelidin <nelidin@yandex.ru>
2023-02-03 15:59:37 +0300

    add task_13

---
+++ task_13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//
+//                                            645 -> 5
+//                                            78 -> третьей цифры нет
+//                                            32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Введите любое число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 100)
+
+{
+    Console.WriteLine($"Третьeй цифры нет");
+}
+
+while (num >= 100)
+
+{
+    if (num < 1000)
+
+    {
+        int result = num % 10;
+        Console.WriteLine($"Третья цифра = {result}");
+    }
+
+    num = num / 10;
+}
